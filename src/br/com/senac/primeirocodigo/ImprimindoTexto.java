413c0f6b9b8ed2216f998c651c465e5774236dfb
guiviniciuss <guilhermevsdsilva@gmail.com>
2022-07-09 17:14:33 -0300

    first commit

---
+++ src/br/com/senac/primeirocodigo/ImprimindoTexto.java
@@ -0,0 +1,21 @@
+package br.com.senac.primeirocodigo;
+
+public class ImprimindoTexto {
+
+	public static void main(String[] args) {
+		// Texto que será impresso no console.
+		System.out.println("Eu sou um excelente desenvolvedor, "
+				+ "serei um excelente profissional, "
+				+ "preciso estudar muito, "
+				+ "serei um eterno aprendiz "
+				+ "e só sei que nada sei, "
+				+ "porque no livro da humildade "
+				+ "a primeira assinatura tem que "
+				+ "ser minha.\n");
+		
+		// Cálculo matemático, o resultado será impresso no console.
+		System.out.println(77*77);
+
+	}
+
+}
